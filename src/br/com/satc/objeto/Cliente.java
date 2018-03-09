5a2c4f041de26e98f4c26da0047b789ef635266c
menin <menin@10.32.137.140>
2018-03-09 16:28:26 -0300

    Iniciada Classe Pedido, feito os métodos, Singleton;

--- src/br/com/satc/objeto/Cliente.java
+++ src/br/com/satc/objeto/Cliente.java
@@ -4,8 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class Cliente extends Pessoa{
-    
+public class Cliente extends Pessoa {
+
     private Date dataCadastro;
     private String numeroCelular;
     private int tempoCliente;
@@ -44,36 +44,26 @@ public class Cliente extends Pessoa{
     @Override
     public String toString() {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        return "Cliente{" + "dataCadastro=" + sdf.format(dataCadastro)+ ", numeroCelular=" + numeroCelular + ", tempoCliente=" + tempoCliente + '}';
+        return "Cliente{" + "dataCadastro=" + sdf.format(dataCadastro) + ", numeroCelular=" + numeroCelular + ", tempoCliente=" + tempoCliente + '}';
     }
 
-    
-     public static int calculatempo(java.util.Date dataCadastro) {
+    public static int calculatempo(java.util.Date dataCadastro) {
 
-    Calendar dataNascimento = Calendar.getInstance();  
-    dataNascimento.setTime(dataCadastro); 
-    Calendar hoje = Calendar.getInstance();  
+        Calendar dataNascimento = Calendar.getInstance();
+        dataNascimento.setTime(dataCadastro);
+        Calendar hoje = Calendar.getInstance();
 
-    int tempoCliente = hoje.get(Calendar.YEAR) - dataNascimento.get(Calendar.YEAR); 
+        int tempoCliente = hoje.get(Calendar.YEAR) - dataNascimento.get(Calendar.YEAR);
 
-    if (hoje.get(Calendar.MONTH) < dataNascimento.get(Calendar.MONTH)) {
-      tempoCliente--;  
-    } 
-    else 
-    { 
-        if (hoje.get(Calendar.MONTH) == dataNascimento.get(Calendar.MONTH) && hoje.get(Calendar.DAY_OF_MONTH) < dataNascimento.get(Calendar.DAY_OF_MONTH)) {
-            tempoCliente--; 
+        if (hoje.get(Calendar.MONTH) < dataNascimento.get(Calendar.MONTH)) {
+            tempoCliente--;
+        } else {
+            if (hoje.get(Calendar.MONTH) == dataNascimento.get(Calendar.MONTH) && hoje.get(Calendar.DAY_OF_MONTH) < dataNascimento.get(Calendar.DAY_OF_MONTH)) {
+                tempoCliente--;
+            }
         }
+
+        return tempoCliente;
     }
 
-    return tempoCliente;
-}
-     
-   
-    
-    
-    
-    
-    
-    
 }
--- src/br/com/satc/objeto/Pedido.java
+++ src/br/com/satc/objeto/Pedido.java
@@ -5,6 +5,7 @@
  */
 package br.com.satc.objeto;
 
+import br.com.satc.singleton.SCliente;
 import java.util.ArrayList;
 
 /**
@@ -12,10 +13,81 @@ import java.util.ArrayList;
  * @author menin
  */
 public class Pedido {
+
     private ArrayList<Prato> pratos;
     private Cliente cliente;
-    private float valotTotal;
+    private float valorTotal;
     private char status;
     private float desconto;
-}
 
+    public Pedido(Cliente cliente, char status, Prato prato) {
+        this.pratos = new ArrayList<>();
+        this.pratos.add(prato);
+        this.cliente = cliente;
+        this.valorTotal = valorTotal;
+        this.status = 'A';
+        this.desconto = desconto;
+    }
+
+    public ArrayList<Prato> getPratos() {
+        return pratos;
+    }
+
+    public void setPratos(ArrayList<Prato> pratos) {
+        this.pratos = pratos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valotTotal) {
+        this.valorTotal = valotTotal;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public float getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(float desconto) {
+        this.desconto = desconto;
+    }
+
+    public static float calcularValorTotal() {
+        Pedido p = null;
+        float valortotal = calcularDesconto() * p.valorTotal;
+        return valortotal;
+    }
+
+    public static float calcularDesconto() {
+        Cliente c = null;
+        float desconto = c.getTempoCliente() * 10;
+        desconto = desconto / 100;
+        desconto = 1 - desconto;
+        return desconto;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Pratos: " + pratos + ", cliente: " + cliente
+                + ", valotTotal: " + valorTotal + ", desconto: " + desconto + '.';
+    }
+
+}
