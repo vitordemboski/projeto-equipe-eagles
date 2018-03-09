4eef5cc7b331d147e5e3e88089628034d045ba64
menin <menin@10.32.137.140>
2018-03-09 14:39:40 -0300

    Iniciada Classe Pedido

---
+++ src/br/com/satc/Objetos/Pedidos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.satc.Objetos;
+
+/**
+ *
+ * @author menin
+ */
+public class Pedidos {
+    private Pratos ArrayList<Pratos>;
+    private Clientes cliente;
+}
